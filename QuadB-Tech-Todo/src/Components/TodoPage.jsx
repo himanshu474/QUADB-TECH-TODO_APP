e4c9aeee4175aa970ac9cf7bf985cab6bda069e0
Himanshu Rawat <himanshurawat474@gmail.com>
2024-04-02 19:52:35 +0530

    commit

---
+++ QuadB-Tech-Todo/src/Components/TodoPage.jsx
@@ -0,0 +1,39 @@
+import { useDispatch } from "react-redux";
+import {
+  Checkbox,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+import { Delete } from "@material-ui/icons";
+import { toggleTodo, deleteTodo } from "../redux/todosSlice";
+
+const TodoPage = ({ id, text, completed }) => {
+  const dispatch = useDispatch();
+
+  const handleToggle = () => {
+    dispatch(toggleTodo(id));
+  };
+
+  const handleDelete = () => {
+    dispatch(deleteTodo(id));
+  };
+
+  return (
+    <ListItem
+      button
+      onClick={handleToggle}
+      style={{ textDecoration: completed ? "line-through" : "none" }}
+    >
+      <ListItemIcon>
+        <Checkbox checked={completed} />
+      </ListItemIcon>
+      <ListItemText primary={text} />
+      <ListItemIcon>
+        <Delete onClick={handleDelete} />
+      </ListItemIcon>
+    </ListItem>
+  );
+};
+
+export default TodoPage;
